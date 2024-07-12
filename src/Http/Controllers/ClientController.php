49d64bc55955d4e3a79a634ec96a997405210334
Kani Robinson <me@kanirobinson.co.uk>
2024-07-12 14:58:24 +0100

    Update auth_ticket on every request

--- src/Http/Controllers/ClientController.php
+++ src/Http/Controllers/ClientController.php
@@ -2,16 +2,21 @@
 
 namespace Atom\Theme\Http\Controllers;
 
-use Illuminate\Routing\Controller;
 use Illuminate\View\View;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
 
 class ClientController extends Controller
 {
     /**
      * Handle an incoming request.
      */
-    public function __invoke(): View
+    public function __invoke(Request $request): View
     {
+        $request->user()->update([
+            'auth_ticket' => str()->uuid(),
+        ]);
+
         return view('client');
     }
 }
