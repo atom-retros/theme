3943b02eecb5577e24c473e639d9567b3ed41d64
KaniRobinson <me@kanirobinson.co.uk>
2024-08-02 23:08:57 +0100

    Added permissions to Habbo Home Categories and Items

--- src/Http/Controllers/Api/HomeCategoryController.php
+++ src/Http/Controllers/Api/HomeCategoryController.php
@@ -2,19 +2,21 @@
 
 namespace Atom\Theme\Http\Controllers\Api;
 
-use Atom\Core\Models\WebsiteHomeCategory;
-use Atom\Theme\Http\Resources\WebsiteHomeCategoryResource;
+use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Routing\Controller;
+use Atom\Core\Models\WebsiteHomeCategory;
+use Atom\Theme\Http\Resources\WebsiteHomeCategoryResource;
 
 class HomeCategoryController extends Controller
 {
     /**
      * Handle the incoming request.
      */
-    public function __invoke(): JsonResponse
+    public function __invoke(Request $request): JsonResponse
     {
         $categories = WebsiteHomeCategory::with(['children' => fn ($query) => $query->has('items')])
+            ->where('permission_id', '<=', $request->user()->rank)
             ->whereNull('website_home_category_id')
             ->get();
 
