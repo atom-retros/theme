1289fd0b539908c0308cae7ca7b78b166cfc820d
KaniRobinson <me@kanirobinson.co.uk>
2024-08-02 23:55:16 +0100

    Removed children by rank

--- src/Http/Controllers/Api/HomeCategoryController.php
+++ src/Http/Controllers/Api/HomeCategoryController.php
@@ -15,7 +15,7 @@ class HomeCategoryController extends Controller
      */
     public function __invoke(Request $request): JsonResponse
     {
-        $categories = WebsiteHomeCategory::with(['children' => fn ($query) => $query->has('items')])
+        $categories = WebsiteHomeCategory::with(['children' => fn ($query) => $query->has('items')->where('permission_id', '>=', $request->user()->rank)])
             ->where('permission_id', '>=', $request->user()->rank)
             ->whereNull('website_home_category_id')
             ->get();
