8917abc80e82574ee5cd07b8e2f2cc828b922d2d
KaniRobinson <me@kanirobinson.co.uk>
2024-08-03 00:04:40 +0100

    Reverted

--- src/Http/Controllers/Api/HomeCategoryController.php
+++ src/Http/Controllers/Api/HomeCategoryController.php
@@ -15,7 +15,7 @@ class HomeCategoryController extends Controller
      */
     public function __invoke(Request $request): JsonResponse
     {
-        $categories = WebsiteHomeCategory::with(['children' => fn ($query) => $query->where('permission_id', '>=', $request->user()->rank)->whereHas('items')])
+        $categories = WebsiteHomeCategory::with('children')
             ->where('permission_id', '>=', $request->user()->rank)
             ->whereNull('website_home_category_id')
             ->whereHas('children', fn ($query) => $query->where('permission_id', '>=', $request->user()->rank))
