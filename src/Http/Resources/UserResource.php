0b1da57cba0aaa6d31bc667f4b0f344a0bd705be
KaniRobinson <me@kanirobinson.co.uk>
2024-08-04 20:30:45 +0100

    Fixed  issue when user is not authenticated

--- src/Http/Resources/UserResource.php
+++ src/Http/Resources/UserResource.php
@@ -17,10 +17,10 @@ class UserResource extends JsonResource
         return [
             'id' => $this->id,
             'username' => $this->username,
-            'credits' => $this->when(auth()->user()?->id === $this->id, $this->credits),
+            'credits' => $this->when( $request->user()?->is($this->resource), $this->credits),
             'background' => $this->home_background ?: asset('images/home-bg.gif'),
             'look' => $this->look,
-            'editable' => $request->user()->is($this->resource),
+            'editable' => $request->user()?->is($this->resource),
         ];
     }
 }
