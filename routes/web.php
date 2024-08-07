924e59706c01cb3cf9a3e1c7c168273d17ae1ce8
KaniRobinson <me@kanirobinson.co.uk>
2024-08-07 12:09:27 +0100

    Added voting to be more widespread

--- routes/web.php
+++ routes/web.php
@@ -26,19 +26,19 @@ Route::middleware('web')->group(function () {
         ->name('index');
 
     Route::get('profiles/{user:username}', ProfileController::class)
-        ->middleware(Authenticate::using('sanctum'))
+        ->middleware(Authenticate::using('sanctum'), 'voting.check')
         ->name('profiles');
 
     Route::get('leaderboards', LeaderboardController::class)
-        ->middleware(Authenticate::using('sanctum'))
+        ->middleware(Authenticate::using('sanctum'), 'voting.check')
         ->name('leaderboards');
 
     Route::get('rare-values', RareValueController::class)
-        ->middleware(Authenticate::using('sanctum'))
+        ->middleware(Authenticate::using('sanctum'), 'voting.check')
         ->name('rare-values');
 
     Route::get('shop', ShopController::class)
-        ->middleware(Authenticate::using('sanctum'))
+        ->middleware(Authenticate::using('sanctum'), 'voting.check')
         ->name('shop');
 
     Route::name('game.')->prefix('game')->group(function () {
@@ -53,53 +53,53 @@ Route::middleware('web')->group(function () {
 
     Route::name('users.')->prefix('users')->group(function () {
         Route::get('me', HomeController::class)
-            ->middleware(Authenticate::using('sanctum'))
+            ->middleware(Authenticate::using('sanctum'), 'voting.check')
             ->name('me');
 
         Route::name('settings.')->prefix('settings')->group(function () {
             Route::resource('account', AccountSettingsController::class)
-                ->middleware(Authenticate::using('sanctum'))
+                ->middleware(Authenticate::using('sanctum'), 'voting.check')
                 ->only(['index', 'store']);
 
             Route::resource('password', PasswordController::class)
-                ->middleware(Authenticate::using('sanctum'))
+                ->middleware(Authenticate::using('sanctum'), 'voting.check')
                 ->only(['index', 'store']);
         });
     });
 
     Route::name('help-center.')->prefix('help-center')->group(function () {
         Route::get('/', HelpCentreController::class)
-            ->middleware(Authenticate::using('sanctum'))
+            ->middleware(Authenticate::using('sanctum'), 'voting.check')
             ->name('index');
 
         Route::get('rules', RuleController::class)
-            ->middleware(Authenticate::using('sanctum'))
+            ->middleware(Authenticate::using('sanctum'), 'voting.check')
             ->name('rules');
 
         Route::resource('tickets', TicketController::class)
-            ->middleware(Authenticate::using('sanctum'))
+            ->middleware(Authenticate::using('sanctum'), 'voting.check')
             ->only(['index', 'create', 'store', 'show', 'destroy']);
     });
 
     Route::name('community.')->prefix('community')->group(function () {
         Route::get('teams', TeamController::class)
-            ->middleware(Authenticate::using('sanctum'))
+            ->middleware(Authenticate::using('sanctum'), 'voting.check')
             ->name('teams');
 
         Route::get('staff', StaffController::class)
-            ->middleware(Authenticate::using('sanctum'))
+            ->middleware(Authenticate::using('sanctum'), 'voting.check')
             ->name('staff');
 
         Route::resource('articles', ArticleController::class)
-            ->middleware(Authenticate::using('sanctum'))
+            ->middleware(Authenticate::using('sanctum'), 'voting.check')
             ->only(['index', 'show', 'update']);
 
         Route::resource('staff-applications', StaffApplicationController::class)
-            ->middleware(Authenticate::using('sanctum'))
+            ->middleware(Authenticate::using('sanctum'), 'voting.check')
             ->only(['index', 'show', 'store']);
 
         Route::get('photos', PhotoController::class)
-            ->middleware(Authenticate::using('sanctum'))
+            ->middleware(Authenticate::using('sanctum'), 'voting.check')
             ->name('photos');
     });
 });
